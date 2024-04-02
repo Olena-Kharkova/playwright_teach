c12cf444ec325f34c47fb91d0fa04693a051c5ab
tryam <tryamkin@gmail.com>
2024-04-02 20:56:07 +0300

    lets go - asserts v2

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -15,25 +15,26 @@ public class GoogleSearchTest extends BasePlayWrightTest{
 
     @Step ("Open page and search")
     public void openBrowser(){
-        page.navigate("https://www.google.com.ua");
+        page.navigate("https://www.google.com.ua/");
       // assertThat(page.getByLabel("Пошук", new Page.GetByLabelOptions().setExact(true))).isVisible();
     }
 
     @Step ("Enter text into search filed")
     public void enterIntoSearchField(String search){
-        page.getByLabel("Пошук", new Page.GetByLabelOptions().setExact(true)).fill(search);
+       page.querySelector("//*[@id=\"APjFqb\"]").fill(search);
     }
 
     @Test
     @Description ("Поиск в гугле")
     @Severity(SeverityLevel.CRITICAL)
-    public void searchTest(){
+    public void searchTest() throws InterruptedException {
        openBrowser();
        enterIntoSearchField("hi google");
-        page.getByLabel("Пошук", new Page.GetByLabelOptions().setExact(true)).press("Enter");
-      //   assertThat(page.locator("#result-stats")).containsText("Приблизна кількість результатів:");
-      //  assertThat(page.locator("#center_col")).containsText("google");
+        page.querySelector("//*[@id=\"APjFqb\"]").press("Enter");
+        assertThat(page.locator("#result-stats")).containsText("Приблизна кількість результатів:");
+        assertThat(page.locator("#center_col")).containsText("google");
 
-        Assert.assertTrue(page.locator("#center_col").isVisible());
+       // page.wait(500);
+      //  Assert.assertEquals(page.locator('#result-stats").contains,"Приблизна"));
     }
 }
